757c652165a27c76e4d1ecd1e6964bc766992560
Austin Moen <moenaustin20@gmail.com>
2020-04-13 11:59:18 -0500

    Initial commit

---
+++ LinqProblem3/GradeLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqProblem3
+{
+    class GradeLibrary
+    {
+        public List<string> GetGrades()
+        {
+            return new List<string>()
+            {
+            "80,100,92,89,65",
+            "93,81,78,84,69",
+            "73,88,83,99,64",
+            "98,100,66,74,55"
+            };
+
+        }
+    }
+}
